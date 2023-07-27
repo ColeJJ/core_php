fa8fce3c631261bcf19df2d17e89af90f1692fa5
ColeJJ <torben.unland@gmail.com>
2023-07-27 13:48:18 +0200

    requiring Config in the initialisation of the backend

--- init.php
+++ init.php
@@ -1,6 +1,7 @@
 <?php
 
 // we require Autoloader first because its a class itself
+require_once __DIR__."/config/Config.php";
 require_once __DIR__."/core/Autoloader.php";
 require_once __DIR__."/core/Bootstrap.php";
 
