a7ccdc0a0d8441b190a7c6b16e1ddc611e9bf52e
ColeJJ <torben.unland@gmail.com>
2023-07-27 13:46:34 +0200

    added config file with global env and configuration settings

---
+++ config/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+class Config {
+	// DB ENV 
+	public static $DB_HOST = "127.0.0.1"; 
+	public static $DB_DB = "test"; 
+	public static $DB_USER = "root"; 
+	public static $DB_PASSWORD = "qwer1234"; 
+}
