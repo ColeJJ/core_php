d758bfd0dadb8a39e65add9f4f048ce633d7f2a7
ColeJJ <torben.unland@gmail.com>
2023-07-30 15:08:33 +0200

    added __DIR__ for scripts autoloading since they somehow need this

--- core/src/Autoloader.php
+++ core/src/Autoloader.php
@@ -8,19 +8,18 @@ class Autoloader {
 		spl_autoload_register(function($class) {
 			$class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 			$class_src = substr_replace($class, "src/", strpos($class, '/') + 1, 0);
-			$class_src = $class_src.'.php';
+			$class_src = __DIR__.'/../../'.$class_src.'.php';
 
 			if(file_exists($class_src)){
 				require $class_src;
 				return;
 			}
-
 		});
 
 		self::registerPackages();
 	}	
 
 	private static function registerPackages() {
-		require_once $_SERVER["DOCUMENT_ROOT"]."/vendor/autoload.php";
+		require_once __DIR__."./../../vendor/autoload.php";
 	} 
 }
