191f23189d04a10f3724c24a944de9c74790dde8
ColeJJ <torben.unland@gmail.com>
2023-09-10 14:50:07 +0200

    added column propterties to SQL class

--- core/src/database/SQL.php
+++ core/src/database/SQL.php
@@ -3,7 +3,12 @@
 namespace Core\Database;
 
 class SQL {
+	// Data types
 	public const VARCHAR30 = "VARCHAR(30)";
 	public const VARCHAR50 = "VARCHAR(50)";
 	public const INT60 = "INT(60)";
+
+	// column properties
+	public const UNIQUE = "UNIQUE";
+	public const FK = "Fk";
 }
