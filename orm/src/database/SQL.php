cd42b9a47a9c3f6f5554061005460a75857cc7ba
ColeJJ <torben.unland@gmail.com>
2023-12-27 21:01:13 +0100

    added todo

--- orm/src/database/SQL.php
+++ orm/src/database/SQL.php
@@ -16,6 +16,7 @@ class SQL {
 	}
 
 	// Data types
+	// todo: export this into a seperate enum or class 
 	public const VARCHAR30 = "VARCHAR(30)";
 	public const VARCHAR50 = "VARCHAR(50)";
 	public const INT = "INT";
