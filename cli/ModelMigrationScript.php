f1afeec88b2642a6cd38a34c93e6c51e6eb0f51e
ColeJJ <torben.unland@gmail.com>
2024-02-20 19:09:14 +0100

    integrated Model meta variable to script since we dont return it anymore

--- cli/ModelMigrationScript.php
+++ cli/ModelMigrationScript.php
@@ -1,22 +1,20 @@
 <?php
 
-use ORM\BaseModel;
 use ORM\Database\Database;
 use ORM\ModelRessources;
- 
+
 require_once __DIR__."/../init/init.php";
 
 $modelClasses = ModelRessources::getDefinedModels();
 
 foreach ($modelClasses as $class) {
 	/**
-	 * @var BaseModel 
-	*/
+	 * @var Model $model 
+ 	*/
 	$model = new $class;
-	$modelMeta = $model->defineORM();
+	$model->defineORM();
+	$model->meta->tablename;
+	$model->meta->columns;
 
-	$tablename = $modelMeta->tablename;
-	$columns = $modelMeta->columns;
-
-	Database::runOrm($modelMeta);
+	Database::runOrm($model->meta);
 }
