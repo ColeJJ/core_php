1cd2aae93c4e2131fe22cef1b1d05d4c530a2960
ColeJJ <torben.unland@gmail.com>
2024-02-20 19:04:49 +0100

    improved autoloading by activaley loading the classes

--- init/src/Autoloader.php
+++ init/src/Autoloader.php
@@ -2,24 +2,31 @@
 
 namespace Init;
 
+use ORM\ModelRessources;
+
 class Autoloader {
 
 	public static function registerClasses() {
 		spl_autoload_register(function($class) {
-			$class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-			$class_src = substr_replace($class, "src/", strpos($class, '/') + 1, 0);
-			$class_src = __DIR__.'/../../'.$class_src.'.php';
-
-			if(file_exists($class_src)){
-				require $class_src;
-				return;
-			}
+			self::register($class);
 		});
 
-		self::registerPackages();
+		$classes = ModelRessources::getDefinedModels();
+
+		foreach ($classes as $class) {
+			self::register($class);
+		}
+
 	}	
 
-	private static function registerPackages() {
-		require_once __DIR__."./../../vendor/autoload.php";
-	} 
+	private static function register($class) {
+		$class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+		$class_src = substr_replace($class, "src/", strpos($class, '/') + 1, 0);
+		$class_src = __DIR__.'/../../'.$class_src.'.php';
+
+		if(file_exists($class_src)){
+			require $class_src;
+			return;
+		}
+	}
 }
